ddb64addff86eb80f1213cd3491fddc459f8b168
andersonguelphjs <andersonguelphjs@gmail.com>
2015-12-19 21:05:50 -0500

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -7,7 +7,7 @@ $.ajax({
         'X-Mashape-Key':'LV9LpSlQramshJQy1fnb6l39pZyep1CWIZcjsnvrW8svsORDNA',
         'Content-Type':'application/json'
     },dataType: 'json',
-    success: function(data) { console.dir((data.source)); },
+    success: function(data) { console.dir(data); },
     error: function(err) { alert(err); },
     beforeSend: function(xhr) {
     xhr.setRequestHeader("X-Mashape-Authorization", "YOUR-MASHAPE-KEY"); // Enter here your Mashape key
