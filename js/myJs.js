3f2d36136afd4a2f79ec69a1e3c7c6f52b4db147
andersonguelphjs <andersonguelphjs@gmail.com>
2015-12-19 21:03:12 -0500

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,5 +1,26 @@
 $(document).ready(function (){
-
+$.ajax({
+    url: 'https://mager-spotify-web.p.mashape.com/search/1/track.json?q=Call%2BMe%2BMaybe', // The URL to the API. You can get this in the API page of the API you intend to consume
+    type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
+    data: {}, // Additional parameters here
+        headers: {
+        'X-Mashape-Key':'LV9LpSlQramshJQy1fnb6l39pZyep1CWIZcjsnvrW8svsORDNA',
+        'Content-Type':'application/json'
+    },dataType: 'json',
+    success: function(data) { console.dir((data.source)); },
+    error: function(err) { alert(err); },
+    beforeSend: function(xhr) {
+    xhr.setRequestHeader("X-Mashape-Authorization", "YOUR-MASHAPE-KEY"); // Enter here your Mashape key
+    }
+});
+/*
+unirest.get("https://mager-spotify-web.p.mashape.com/search/1/track.json?q=Call%2BMe%2BMaybe")
+.header("X-Mashape-Key", "LV9LpSlQramshJQy1fnb6l39pZyep1CWIZcjsnvrW8svsORDNA")
+.header("Accept", "text/plain")
+.end(function (result) {
+  console.log(result.status, result.headers, result.body);
+});
+*/
 $("table").find("td").on("click", function(){
 
 $(this).css("background-color","#e5e5e5");
