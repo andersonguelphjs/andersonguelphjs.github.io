7b9725f66818a7a175be30e50276c5b740ea1faf
andersonguelphjs <andersonguelphjs@gmail.com>
2016-02-17 23:06:08 -0500

    comment

--- otherProjects/ticTacToe/js/ticTacToe.js
+++ otherProjects/ticTacToe/js/ticTacToe.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
 
-  var isO = true,
-    isInProgress = true,
-    combos = {
+  var isO = true, // what does this variable represent
+    isInProgress = true, // what does this variable represent
+    combos = { // what does this variable represent; explain the keys and values
       0: [
         [1, 2],
         [3, 6],
@@ -46,51 +46,54 @@ $(document).ready(function() {
         [6, 7]
       ]
     };
-
+  // Explain what this event does
   $("#board").find("div").on("click", function() {
     var selection;
 
-    if (isInProgress && $(this).hasClass("empty")) {
-      if (isO) {
+    if (isInProgress && $(this).hasClass("empty")) { /// Explain these conditions
+      if (isO) { //Explain this condition
         selection = "o";
-        $(this).removeClass("empty").addClass(selection).append("<span class='circle'>o</span");
+        $(this).removeClass("empty").addClass(selection).append("<span class='circle'>o</span"); //Explain what is happening here
       } else {
         selection = "x";
         $(this).removeClass("empty").addClass(selection).append("<span class='eX'>x</span>");
       }
-      isO = !isO;
-      checkIfWon($(this).index(), selection);
+      isO = !isO; //Explain
+      checkIfWon($(this).index(), selection); //Explain
     }
 
   });
 
+  // Explain what this event does
   $("#newGame").on("click", function() {
 
     var divs = $("#board").find("div");
 
+    //Explain this each function
     divs.each(function() {
       $(this).removeClass("o").removeClass("x").addClass("empty").empty();
-
     })
     isInProgress = true;
   })
 
+  //Explain this funciton, describe the parameters; what are the possible values of the paramaters
   function checkIfWon(i, selection) {
 
     var arr = combos[i],
       valid;
-    if (arr && arr.length > 0) {
-      for (var i = 0; i < arr.length; i++) {
+
+    if (arr && arr.length > 0) { //Explain this condition; is it really necessary?
+      for (var i = 0; i < arr.length; i++) { //Explain this nested for loop
         valid = true;
         for (var j = 0; j < arr[i].length; j++) {
-          if (!$("#board div").eq(arr[i][j]).hasClass(selection)) {
+          if (!$("#board").find("div").eq(arr[i][j]).hasClass(selection)) { //Explain this condition
             valid = false;
           }
         }
-        if (valid) {
-          alert("Winner is " + selection + "!");
+        if (valid) { //Explain the condition and every line in the block
+          alert("Winner is " + selection.toUpperCase() + "!");
           isInProgress = false;
-          return false;
+          return false; //this exits the loop
         }
       }
     }
