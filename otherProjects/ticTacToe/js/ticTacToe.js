acbae9c0df51d3ce2f56b2d922b9b8d9b16eed23
andersonguelphjs <andersonguelphjs@gmail.com>
2016-02-19 22:24:59 -0500

    final

--- otherProjects/ticTacToe/js/ticTacToe.js
+++ otherProjects/ticTacToe/js/ticTacToe.js
@@ -1,65 +1,67 @@
 $(document).ready(function() {
 
-  var isO = true, // what does this variable represent
-    isInProgress = true, // what does this variable represent
-    combos = { // what does this variable represent; explain what the keys and values represent
-      0: [
-        [1, 2],
-        [3, 6],
-        [4, 8]
-      ],
-      1: [
-        [0, 2],
-        [4, 7]
-      ],
-      2: [
-        [0, 1],
-        [5, 8],
-        [4, 6]
-      ],
-      3: [
-        [0, 6],
-        [4, 5]
-      ],
-      4: [
-        [1, 8],
-        [2, 6],
-        [1, 7],
-        [3, 5]
-      ],
-      5: [
-        [2, 8],
-        [3, 4]
-      ],
-      6: [
-        [0, 3],
-        [2, 4],
-        [7, 8]
-      ],
-      7: [
-        [1, 4],
-        [6, 8]
-      ],
-      8: [
-        [0, 4],
-        [2, 5],
-        [6, 7]
-      ]
-    };
+  var isCircle = true; // what does this variable represent
+  var isGameInProgress = true; // what does this variable represent
+  var winningCombos = { // what does this variable represent; explain what the keys and values represent
+    0: [ //0 is key
+      [1, 2], //this multiDimensional Array is values
+      [3, 6],
+      [4, 8]
+    ],
+    1: [
+      [0, 2],
+      [4, 7]
+    ],
+    2: [
+      [0, 1],
+      [5, 8],
+      [4, 6]
+    ],
+    3: [
+      [0, 6],
+      [4, 5]
+    ],
+    4: [
+      [1, 8],
+      [2, 6],
+      [1, 7],
+      [3, 5]
+    ],
+    5: [
+      [2, 8],
+      [3, 4]
+    ],
+    6: [
+      [0, 3],
+      [2, 4],
+      [7, 8]
+    ],
+    7: [
+      [1, 4],
+      [6, 8]
+    ],
+    8: [
+      [0, 4],
+      [2, 5],
+      [6, 7]
+    ]
+  };
+
   // Explain what this event does
   $("#board").find("div").on("click", function() {
-    var selection;
 
-    if (isInProgress && $(this).hasClass("empty")) { /// Explain these conditions
-      if (isO) { //Explain this condition
-        selection = "o";
-        $(this).removeClass("empty").addClass(selection).append("<span class='circle'>o</span"); //Explain what is happening here
+    var circleOrEx;
+
+    if (isGameInProgress && $(this).hasClass("empty")) { /// Explain these conditions
+      if (isCircle) { //Explain this condition
+        circleOrEx = "o";
+        $(this).removeClass("empty").addClass(circleOrEx).append("<span class='circle'>o</span"); //Explain what is happening here
       } else {
-        selection = "x";
-        $(this).removeClass("empty").addClass(selection).append("<span class='eX'>x</span>");
+        circleOrEx = "x";
+        $(this).removeClass("empty").addClass(circleOrEx).append("<span class='eX'>x</span>");
       }
-      isO = !isO; //Explain
-      checkIfWon($(this).index(), selection); //Explain
+      isCircle = !isCircle; //Explain
+      checkIfWon($(this).index(), circleOrEx); //Explain
     }
 
   });
@@ -67,43 +69,49 @@ $(document).ready(function() {
   // Explain what this event does
   $("#newGame").on("click", function() {
 
-    var divs = $("#board").find("div"),
-      emptyDiv = $(".container").find(".nine").filter(function() { //bonus Explain what each one of these functions does
-        return $.trim($(this).text()) === "" && $(this).children().length === 0;
-      }).not("board").first();
+    var divs = $("#board").find("div"); //what is this variable
+    var emptyDiv = $(".container").find(".nine").filter(function() { //bonus Explain what filter does
+      return $.trim($(this).text()) === "" && $(this).children().length === 0;
+    }).not("#board").first();
 
     if (emptyDiv.length == 1) { //what is this if statement doing?
       emptyDiv.html($("#board").html());
     } else {
       $(".container").find(".nine").not("#board").empty();
+      $(".container").find(".nine").first().html($("#board").html());
     }
 
     //Explain this each function
     divs.each(function() {
       $(this).removeClass("o").removeClass("x").addClass("empty").empty();
     })
-    isInProgress = true;
+    isGameInProgress = true;
   })
 
   //Explain this funciton, describe the parameters; what are the possible values of the paramaters
-  function checkIfWon(i, selection) {
+  function checkIfWon(i, circleOrEx) {
 
-    var arr = combos[i],
-      valid;
+    var mulitArr = winningCombos[i];
+    var playerWon;
 
-    if (arr && arr.length > 0) { //Explain this condition; is it really necessary?
-      for (var i = 0; i < arr.length; i++) { //Explain this nested for loop
-        valid = true;
-        for (var j = 0; j < arr[i].length; j++) {
-          if (!$("#board").find("div").eq(arr[i][j]).hasClass(selection)) { //Explain this condition
-            valid = false;
+    if (mulitArr && mulitArr.length > 0) { //Explain this condition; is it really necessary?
+      for (var i = 0; i < mulitArr.length; i++) { //Explain this nested for loop
+        playerWon = true;
+        for (var j = 0; j < mulitArr[i].length; j++) {
+          if (!$("#board").find("div").eq(mulitArr[i][j]).hasClass(circleOrEx)) { //Explain this condition
+            playerWon = false;
           }
         }
-        if (valid) { //Explain the condition and every line in the block
-          alert("Winner is " + selection.toUpperCase() + "!");
-          $("#board").find("." + selection).css("color", "green");
-          isInProgress = false;
-          return false; //this exits the loop
+
+        if (playerWon) { //Explain the condition and every line in the block
+          alert("Winner is " + circleOrEx.toUpperCase() + "!");
+          if (circleOrEx == "o") {
+            $("#board").find(".circle").css("color", "green");
+          } else {
+            $("#board").find(".eX").css("color", "green");
+          }
+          isGameInProgress = false;
+          return false; //this exits the loop 
         }
       }
     }
