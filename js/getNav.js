127d52818856b54e7b2630c92819a4f10d33b4a7
andersonguelphjs <andersonguelphjs@gmail.com>
2016-03-10 11:01:11 -0500

    nav

--- js/getNav.js
+++ js/getNav.js
@@ -3,12 +3,12 @@ $(document).ready(function() {
 //get the file (replace with your own url)
   $.get("http://andersonguelphjs.github.io/partials/nav.html", function(data) {
 
-//check if this document has .conatiner
+//check if this document has .container
     var container = $(".container");
 
     //if so add nav.html contents to the top
     if (container) {
-      $(".container").prepend(data);
+      $(".container").prepend(data).fadeIn();
     }
 
   });
