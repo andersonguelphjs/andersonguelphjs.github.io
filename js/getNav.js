cc8df3f2a548d4e7768fdeba9f06fb241790e08b
andersonguelphjs <andersonguelphjs@gmail.com>
2016-03-14 16:40:05 -0400

    changes

--- js/getNav.js
+++ js/getNav.js
@@ -1,9 +1,9 @@
-$(document).ready(function() {
-
 //get the file (replace with your own url)
-  $.get("http://andersonguelphjs.github.io/partials/nav.html", function(data) {
+$.get("http://andersonguelphjs.github.io/partials/nav.html", function(data) {
 
-//check if this document has .container
+  $(document).ready(function() {
+
+    //check if this document has .container
     var container = $(".container");
 
     //if so add nav.html contents to the top
