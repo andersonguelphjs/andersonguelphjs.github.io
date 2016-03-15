c7942ac3fb3327b7c049775e3a340d686d2bd94a
andersonguelphjs <andersonguelphjs@gmail.com>
2016-03-15 12:03:57 -0400

    sync

--- otherProjects/ajaxExamples/js/clients.js
+++ otherProjects/ajaxExamples/js/clients.js
@@ -5,10 +5,20 @@ $(document).ready(function() {
         var url = "http://andersonguelphjs.github.io/otherProjects/ajaxExamples/jsonDatabase/clients.json";
 
         $.getJSON(url, function(data) {
+            var html = "<table class='table table-hover table-striped'>" +
+              "<tr><th>Name</th><th>Email</th><th>Company</th></tr>";
 
-            $.each(data, function(index, item){
-                $("#data").append(item.name);
+            $.each(data, function(index, item) {
+
+              html += "<tr>" +
+                "<td>" + item.name + "</td>" +
+                "<td>" + item.email + "</td>" +
+                "<td>" + item.company + "</td>" +
+                "</tr>";
             })
+
+            html += "</table>";
+            $("#data").append(html);
             //alert(data);
             //console.dir(data);
 
