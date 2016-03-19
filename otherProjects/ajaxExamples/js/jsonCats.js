37a5dcc5175025f103d83f7bd0a904d98b3279b6
andersonguelphjs <andersonguelphjs@gmail.com>
2016-03-19 12:52:12 -0400

    s

---
+++ otherProjects/ajaxExamples/js/jsonCats.js
@@ -0,0 +1,15 @@
+$.getJSON("http://andersonguelphjs.github.io/otherProjects/ajaxExamples/jsonDatabase/cat.json", function(data){
+
+$(document).ready(function(data){
+
+$("#cats").append(data);
+
+})
+
+})
+
+/*
+<div class="col-md-12">
+  Lorem Ipsum
+</div>
+*/
