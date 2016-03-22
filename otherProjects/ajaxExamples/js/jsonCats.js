8eac01faa39883af854814153df9581700ab5959
andersonguelphjs <andersonguelphjs@gmail.com>
2016-03-22 12:20:14 -0400

    changed

--- otherProjects/ajaxExamples/js/jsonCats.js
+++ otherProjects/ajaxExamples/js/jsonCats.js
@@ -8,11 +8,32 @@ $(document).ready(function() {
       $.each(data, function(index, item) {
           html += '<div class="col-md-4">' +
             '<div class="catName">' + item.name + '</div>' +
-            '<div class="catType">' + item.type + '</div>' +
-            '<div class="catGender">' + item.gender + '</div>' +
-            '<img src="' + item.image + '"/>';
+            '<div class="catType"><small>type </small>' + item.type + '</div>' +
+            '<div class="catGender"><small>gender </small>' + item.gender + '</div>' +
+            '<img class="catImage" src="' + item.image + '"/>' +
+            //deleted commentsContainer
+            '<div class="panel panel-default">' + //added
+            '<div class="panel-heading">Renter Comments</div>'; //added
+          $.each(item.comments, function(ind, i) {
+              html += '<div class="panel-body">' + //added
+                '<div class="renterName">' + i.username + '</div>' +
+                '<div class="renterComment">' + i.comment + '</div>' +
+                '<div class="renterStars">';
 
-          html += '</div>';
+              for (var j = 1; j <= 5; j++) {
+
+                if (j <= i.stars) {
+                  html += '<img src="images/fullStar.png"/>';
+                } else {
+                  html += '<img src="images/emptyStar.png"/>';
+                }
+              }
+              html += '</div>'+//end stars
+                      '</div>'; //panel body
+            }) //each comment
+
+          html += '</div>' + //panel
+            '</div>'; //col-md-4
         }) //each cat
 
       $("#catData").append(html);
@@ -28,10 +49,22 @@ $(document).ready(function() {
     <div class="commentsContainer">
       //one per comment
       <div class="renterName"></div>
-      <div class="renterLocation"></div>
+      <div class="renterComment"></div>
       <div class="renterStars">
       //5 stars, some full, some empty
       </div> //end starts
     </div>//end commentsContainer
   </div>//end cat
+
+  <div class="panel panel-default">
+
+    <div class="panel-heading">Renter Comments/div>
+
+    <div class="panel-body">
+    //name
+    //comment
+    //stars
+    </div>
+
+  </div>
   */
