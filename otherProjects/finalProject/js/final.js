35f816fe8d6ae1f1a8c94c74e19366e7c318bedd
andersonguelphjs <andersonguelphjs@gmail.com>
2016-03-25 00:17:22 -0400

    master

--- otherProjects/finalProject/js/final.js
+++ otherProjects/finalProject/js/final.js
@@ -16,7 +16,60 @@ console.log(page);
           $.get("partials/order.html", function(data) {
 
             $("#pageContent").html(data);
+            $("#myButton").on("mouseenter", function() {
+                $("#log").append("<br>Button mouseenter");
+                $(this).text("ORDER NOW!!!");
+              })
+              .on("mouseleave", function() {
+                $("#log").append("<br>Button mouseleave");
+                $(this).text("Click Me!");
+              });
 
+
+            //change the backgrund color on focus, blue
+            $("#mySingleLineText").on("focus", function() {
+                $("#log").append("<br>input focus");
+                $(this).css("background-color", "#F7F8E0");
+              })
+              .on("blur", function() {
+                $("#log").append("<br>input blur");
+                $(this).css("background-color", "#FFF");
+              });
+
+            //give the user a message about their selection
+            $("#mySelect").on("change", function() {
+
+              var val = $(this).val();
+              $("#log").append("<br>select change");
+              $("#mySelectMessage").html(val + " is a nice selection!");
+
+            });
+
+            //user clicks the button
+            $("#myButton").on("click", function() {
+
+              $("#log").append("<br>User clicked the button");
+
+              var userOrder = {};
+
+              userOrder.myInput = $("#mySingleLineText").val();
+              userOrder.myTextarea = $("#myTextarea").val();
+              userOrder.mySelect = $("#mySelect").val();
+              userOrder.myRadio = $("[name='gender']:checked").val();
+              userOrder.myCheckValues = [];
+
+              $("[name='vehicle']:checked").each(function() {
+                userOrder.myCheckValues.push($(this).val());
+              });
+
+              $("#log").append("<br>Value of input is: " + userOrder.myInput);
+              $("#log").append("<br>Value of textarea is: " + userOrder.myTextarea);
+              $("#log").append("<br>Value of select is: " + userOrder.mySelect);
+              $("#log").append("<br>Value of radio button is: " + userOrder.myRadio);
+              $("#log").append("<br>Value of checks is: " + userOrder.myCheckValues.join());
+              $("#log").append("<br><br>Value of userOrder is: " + JSON.stringify(userOrder));
+
+            })
           });
     }
     else if (page == "models") {
@@ -57,6 +110,7 @@ console.log(page);
           }) //each cat
 
         $("#pageContent").html(html);
+
       })
     }
   }
@@ -68,6 +122,7 @@ var id = $(this).attr("id");
 getPartial(id);
 
 })
+
 
   getPartial("home");
 
